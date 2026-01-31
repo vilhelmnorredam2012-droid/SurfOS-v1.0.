b02922042f428d0cf4922234d9ae8affee284664
Hg8yue <246245603+vilhelmnorredam2012-droid@users.noreply.github.com>
2026-01-31 21:06:49 +0100

    Create mkimg.surfos.sh

---
+++ mkimg.surfos.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# Alpine mkimage profil for SurfOS kiosk
+# Kører med sudo ./mkimg.surfos.sh
+
+PROFILE=surfos
+ALPINE_BRANCH=edge  # eller "v3.20" for stabil
+ARCH=x86_64
+
+apk add alpine-sdk alpine-make-vm-image
+
+cat <<EOF > /tmp/mkimg.$PROFILE.sh
+profile_surfos() {
+  kernel_cmdline="unionfs_size=512M console=tty0 console=ttyS0,115200"
+  syslinux_serial="0 115200"
+  kernel_addons=""
+  apks="alpine-base musl openrc
+        chromium xorg-server xf86-video-vesa xf86-input-libinput
+        xinit dbus openbox unclutter
+        ttf-dejavu font-noto"
+  local _k _a
+  for _k in \$kernel_addons; do apks="\$apks linux-\${_k%-*}-modules-\${_k##*-}"; done
+  for _a in \$kernel_addons; do apks="\$apks \$_a"; done
+}
+
+profile_surfos
+EOF
+
+sudo alpine-make-vm-image \
+  --image-format iso \
+  --image-size 2G \
+  --repositories-file /etc/apk/repositories \
+  --packages "$(cat /tmp/mkimg.$PROFILE.sh | grep apks= | cut -d'"' -f2)" \
+  --profile surfos \
+  --tag surfos \
+  --out surfos-hybrid.iso \
+  overlay/
+
+echo "ISO bygget: surfos-hybrid.iso"
