14e4fb4d4e572f7be28c0c0f29d81e13a1e49296
Hg8yue <246245603+vilhelmnorredam2012-droid@users.noreply.github.com>
2026-02-01 10:11:53 +0100

    Update mkimg.surfos.sh

--- mkimg.surfos.sh
+++ mkimg.surfos.sh
@@ -4,33 +4,39 @@
 # Køres inde i Docker som root – ingen sudo nødvendigt
 
 PROFILE=surfos
-ALPINE_BRANCH=edge  # eller "v3.21" for stabil version
+ALPINE_BRANCH=edge  # eller "v3.20" for stabil
 ARCH=x86_64
 
+# Lav profile-scriptet
 cat <<EOF > /tmp/mkimg.$PROFILE.sh
 profile_surfos() {
   kernel_cmdline="unionfs_size=512M console=tty0 console=ttyS0,115200"
   syslinux_serial="0 115200"
-  kernel_addons=""
+  kernel_addons="linux-lts"  # Tilføjet kernel for boot
   apks="alpine-base musl openrc
         chromium xorg-server xf86-video-vesa xf86-input-libinput
         xinit dbus openbox unclutter
         ttf-dejavu font-noto"
   local _k _a
-  for _k in \$kernel_addons; do apks="\$apks linux-\${_k%-*}-modules-\${_k##*-}"; done
+  for _k in \$kernel_addons; do apks="\$apks linux-\$_k"; done
   for _a in \$kernel_addons; do apks="\$apks \$_a"; done
 }
+
 profile_surfos
 EOF
 
+# Kør alpine-make-vm-image med korrekt syntax (ingen --profile, --tag, --out – brug <image> som argument)
 alpine-make-vm-image \
   --image-format iso \
   --image-size 2G \
-  --repositories-file /etc/apk/repositories \
-  --packages "$(cat /tmp/mkimg.$PROFILE.sh | grep apks= | cut -d'"' -f2)" \
-  --profile surfos \
-  --tag surfos \
-  --out surfos-hybrid.iso \
-  overlay/
+  --arch "$ARCH" \
+  --boot-mode UEFI  # Til hybrid USB (UEFI + BIOS via syslinux)
+  --branch "$ALPINE_BRANCH" \
+  --fs-skel-dir overlay/ \
+  surfos-hybrid.iso \
+  /tmp/mkimg.$PROFILE.sh
+
+# Lav ISO'en hybrid (til USB-boot)
+isohybrid surfos-hybrid.iso
 
 echo "ISO bygget: surfos-hybrid.iso"
